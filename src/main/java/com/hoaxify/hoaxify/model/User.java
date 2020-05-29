238f3c2cb6a7f9bcb3a077adad23a1fda5cc71c6
Grego Dadone <grego.dadone@gmail.com>
2020-05-29 17:30:37 -0300

    Added UserControllerTest

---
+++ src/main/java/com/hoaxify/hoaxify/model/User.java
@@ -0,0 +1,10 @@
+package com.hoaxify.hoaxify.model;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private String username;
+    private String displayName;
+    private String password;
+}
---
+++ src/test/java/com/hoaxify/hoaxify/UserControllerTest.java
@@ -0,0 +1,22 @@
+package com.hoaxify.hoaxify;
+
+import com.hoaxify.hoaxify.model.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+public class UserControllerTest {
+
+    @Test
+    public void postUser_whenUserIsValid_receiveOk() {
+        User user = new User();
+        user.setUsername("test-user");
+        user.setDisplayName("test-display");
+        user.setPassword("P4ssword");
+    }
+}
