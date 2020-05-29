a6bd66405de0dda5aebcfb0dda9003adf5172967
Grego Dadone <grego.dadone@gmail.com>
2020-05-29 17:46:23 -0300

    minor change

--- src/test/java/com/hoaxify/hoaxify/UserControllerTest.java
+++ src/test/java/com/hoaxify/hoaxify/UserControllerTest.java
@@ -3,7 +3,9 @@ package com.hoaxify.hoaxify;
 import com.hoaxify.hoaxify.model.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -11,6 +13,13 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
 public class UserControllerTest {
+
+    private final TestRestTemplate testRestTemplate;
+
+    @Autowired
+    public UserControllerTest(TestRestTemplate testRestTemplate) {
+        this.testRestTemplate = testRestTemplate;
+    }
 
     @Test
     public void postUser_whenUserIsValid_receiveOk() {
