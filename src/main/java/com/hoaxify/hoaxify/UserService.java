ceef012bbac2c71fdfaf06ef2791e479bbff3241
Grego Dadone <grego.dadone@gmail.com>
2020-06-03 16:25:17 -0300

    excluded security autoconfiguration

--- src/main/java/com/hoaxify/hoaxify/UserService.java
+++ src/main/java/com/hoaxify/hoaxify/UserService.java
@@ -2,19 +2,23 @@ package com.hoaxify.hoaxify;
 
 import com.hoaxify.hoaxify.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public User save(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
 }
